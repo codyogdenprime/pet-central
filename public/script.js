34aa700c3f93fd9dc9061d093258998820ea5bd7
Cody Ogden <cody@codyogden.com>
2016-09-23 14:32:05 -0500

    Angular Working

--- public/script.js
+++ public/script.js
@@ -1,2 +1,2 @@
-
+var myApp=angular.module("myApp",[]);myApp.controller("mainView",["$scope",function(a){console.log("NG")}]);
 //# sourceMappingURL=script.js.map
---
+++ src/js/000_client.js
@@ -0,0 +1,7 @@
+var myApp = angular.module( 'myApp', [] );
+
+myApp.controller( 'mainView', [ '$scope', function( $scope ) {
+
+	console.log( "NG" );
+
+}]);
