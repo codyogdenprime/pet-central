59655d3edf5e4f9457f55657860fab7a7b671402
Cody Ogden <cody@codyogden.com>
2016-09-24 14:03:31 -0500

    Angular Controllers Added for each view

--- src/js/000_client.js
+++ src/js/000_client.js
@@ -3,15 +3,38 @@ var myApp = angular.module("myApp", ["ngRoute"]);
 myApp.config(["$routeProvider", function($routeProvider){
     $routeProvider.
       when("/home", {
-        templateUrl: "/views/about.htm"
+        templateUrl: "/views/about.htm",
+        controller: 'homeController'
       }).
       when("/add", {
-        templateUrl: "/views/add.htm"
+        templateUrl: "/views/add.htm",
+        controller: 'addController'
       }).
       when("/pets", {
-        templateUrl: "/views/pets.htm"
+        templateUrl: "/views/pets.htm",
+        controller: 'viewController'
       }).
       otherwise({
         redirectTo: "/home"
       });
+}]);
+
+myApp.controller( 'homeController', [ '$scope', function( $scope ) {
+
+	console.log('Home Controller');
+
+}]);
+
+myApp.controller( 'addController', [ '$scope', '$location', function( $scope, $location ) {
+
+	console.log( 'Add Pet Controller' );
+
+	// $location.path( 'pets' ); // Change the location once the pet is added.
+
+}]);
+
+myApp.controller( 'viewController', [ '$scope', function( $scope ) {
+
+	console.log( 'View Pets Controller' );
+
 }]);
