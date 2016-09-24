f20a838e4e6d59a55ff16ee302064fadb533457b
Cody Ogden <cody@codyogden.com>
2016-09-24 14:57:24 -0500

    Client POST to Server working

--- src/js/000_client.js
+++ src/js/000_client.js
@@ -24,9 +24,31 @@ myApp.controller( 'homeController', [ '$scope', function( $scope ) {
 
 }]);
 
-myApp.controller( 'addController', [ '$scope', '$location', function( $scope, $location ) {
+myApp.controller( 'addController', [ '$scope', '$http', function( $scope, $http ) {
 
 	console.log( 'Add Pet Controller' );
+
+	$scope.addPet = function( event ) {
+		console.log( 'Run AddPet' );
+
+		var petObj = {
+			name : $scope.pet_name,
+			age : $scope.pet_age,
+			species : $scope.pet_type,
+			image_url : $scope.pet_photo
+		};
+
+		$http({
+			method: 'POST',
+			url: '/pets',
+			data: petObj
+		}).then( function( result ) {
+
+			console.log( 'Added!', result );
+
+		});
+
+	};
 
 	// $location.path( 'pets' ); // Change the location once the pet is added.
 
